fc83557af38fe15cf1c50c21cdafe1eaddea773c
kholmuminov <74708958+kholmuminov@users.noreply.github.com>
2026-02-26 21:03:12 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
-}
+const CACHE_NAME = 'tadbir2025-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Qo'shimcha fayllar bo'lsa shu yerga qo'shing
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
